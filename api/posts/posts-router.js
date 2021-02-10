52eb29f77c55944cd8bb12ce7db0c3a2623b0007
Stephanie <stephanie.enciso13@gmail.com>
2021-02-09 20:53:39 -0600

    Finished the last 3 endpoints. Met MVP.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,11 +1,11 @@
 // implement your posts router here
-const Post = require('./posts-model');
+const Posts = require('./posts-model');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    Post.find()
+    Posts.find()
       .then(posts => {
           res.status(200).json(posts)
       })
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  Post.findById(req.params.id)
+  Posts.findById(req.params.id)
     .then(post => {
       if (post) {
           res.status(200).json(post); 
@@ -34,6 +34,88 @@ router.get('/:id', (req, res) => {
             message: 'The post information could not be retrieved.'
         })
     });
+});
+
+router.post('/', (req, res) => {
+  Posts.insert(req.body)
+    .then(post => {
+        if (post.title === '' || post.contents === '') {
+            res.status(400).json({
+                message: 'Please provide title and contents for the post.'
+            })
+        } else {
+            res.status(201).json(req.body);
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            message: 'There was an error while saving the post to the database.'
+        });
+    });
+});
+
+router.put('/:id', (req, res) => {
+    const postChange = req.body;
+    const id = req.params.id;
+    Posts.update(id, postChange)
+      .then(post => {
+          if (post) {
+              res.status(200).json(postChange);
+         } else if (postChange.title === '' || postChange.contents === '' ) {
+              res.status(400).json({
+                  message: 'Please provide title and contents for the post.'
+              })
+         } else {
+              res.status(404).json({
+                  message: 'The post with the specified ID does not exist.'
+              });
+          };
+      })
+      .catch(error => {
+          console.log(error);
+          res.status(500).json({
+              message: 'The post information could not be modified.'
+          });
+      });
+});
+
+router.delete('/:id', (req, res) => {
+    Posts.remove(req.params.id)
+      .then(post => {
+          if (post) {
+              res.status(200).json(post);
+          } else {
+              res.status(404).json({
+                  message: 'The post with the specified ID does not exist.'
+              })
+          }
+      })
+      .catch(error => {
+          console.log(error);
+          res.status(500).json({
+              message: 'The post could not be removed.'
+          });
+      });
+});
+
+router.get('/:id/comments', (req, res) => {
+    Posts.findPostComments(req.params.id)
+      .then(postComments => {
+          if (postComments) {
+              res.status(200).json(postComments);
+          } else {
+              res.status(404).json({
+                  message: 'The post with the specified ID does not exist.'
+              });
+          };
+      })
+      .catch(error => {
+          console.log(error);
+          res.status(500).json({
+              message: 'The comments information could not be retrieved.'
+          });
+      });
 });
 
 module.exports = router;
