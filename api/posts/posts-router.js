9c9fe9d6146cb0da458735d7f3c75144a953547e
Stephanie <stephanie.enciso13@gmail.com>
2021-03-09 15:59:11 -0600

    the Put endpoint is working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -53,4 +53,32 @@ router.post('', async (req, res) => {
     }
 })
 
+router.put('/:id', async (req, res) => {
+    const { id } = req.params
+    const { title, contents } = req.body
+    if (!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+         try{
+            const post = await Post.findById(id)
+            if(!post) {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist" 
+                })
+            } else {
+                const updatedPost = await Post.update(id, req.body)
+                const newPost = await Post.findById(id)
+                res.status(200).json(newPost)
+            }
+        } catch (err) {
+            res.status(500).json({
+                message: "The post information could not be modified"
+            })
+        }
+    }
+   
+})
+
 module.exports = router;
